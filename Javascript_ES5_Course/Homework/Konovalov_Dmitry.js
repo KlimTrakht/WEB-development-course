33e9fdd44c2f5d7c31ac48fc093a788add6beeaa
inokqq <inokqq@gmail.com>
2019-07-07 23:49:59 +0300

    homework 1 fix

--- Javascript_ES5_Course/Homework/Konovalov_Dmitry.js
+++ Javascript_ES5_Course/Homework/Konovalov_Dmitry.js
@@ -2,7 +2,7 @@ let year = 200;
 let century;
 
 function getCentury(year) {
-	if (year > = 1 || year < 2017) {
+	if (year >= 1 || year < 2017) {
 		century = Math.ceil(year / 100);
 		console.log(century)
 	} else {

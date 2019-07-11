5c5b9a75cf11390b1712d1b612fd1b16bd06e003
deilr <a@d.lg.ua>
2019-07-11 16:26:47 +0300

    delete console.log and prompt

--- Javascript_ES5_Course/Homework/Lesson_1/Nikolay_Ovcharenko.js
+++ Javascript_ES5_Course/Homework/Lesson_1/Nikolay_Ovcharenko.js
@@ -1,6 +1,3 @@
-var year = +prompt('Введите год от 1 до 2017');
-console.log(getCentury(year));
-
 function getCentury(y) {
     return (typeof year === 'number' && Number.isFinite(year) && !(year % 1) && year >= 1 && year <= 2017) ? Math.ceil(y / 100) : "Год указан не правильно"
 }

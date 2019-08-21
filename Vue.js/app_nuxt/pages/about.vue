d98b60a99cb84f66cff2c872e7a5f973692354f0
Klim Trakht <klim@kultprosvet.net>
2019-08-21 09:46:21 +0300

    Add Nuxt.js Lesson 1 code

---
+++ Vue.js/app_nuxt/pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>About us</h3>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+    <p>Some text about our company</p>
+  </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
---
+++ Vue.js/app_nuxt/pages/users/_id.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>User {{ $route.params.id}}</div>
+</template>
+
+<script>
+    export default {
+        name: "_id",
+      validate({params}){
+          return /^\d+$/.test(params.id)
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
